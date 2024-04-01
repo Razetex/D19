9cc954e2ac4992f7d007f7aa4b70912d7d13dcf4
Razetex <140638485+Razetex@users.noreply.github.com>
2024-04-01 21:23:35 +0300

    Delete mmoportal directory

--- mmoportal/portal/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Categories, Post, Author, Reply
-
-admin.site.register(Categories)
-admin.site.register(Post)
-admin.site.register(Author)
-admin.site.register(Reply)
